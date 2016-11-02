dba22fff122afe7066df49c8aa83d374843e9dd7
yifei <dcxin22@163.com>
2016-11-02 10:39:53 +0800

    second commit

---
+++ Constant.java
@@ -0,0 +1,21 @@
+package com.flyou.girls;
+
+/**
+ * ============================================================
+ * 项目名称：test
+
+ * 创建人：yifei
+
+ * 版本：@version  V1.0
+ * ============================================================
+ **/
+public class Constant {
+    public static  final  String BASE_URL="http://m.xxxiao.com";
+    public static  final  String NEW="/page/";
+    public static  final String XINGGAN="/cat/xinggan/page/";
+    public static  final String SHAONV="/cat/shaonv/page/";
+    public static  final String MRXT="/cat/mrxt/page/";
+    public static  final String SWMT="/cat/swmt/page/";
+    public static  final String WMXZ="/cat/wmxz/page/";
+    public static  final String WALLPAPER="/cat/wallpaper/page";
+}
